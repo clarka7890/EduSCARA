00c4d40aab8606528c67fde10451132501fc4477
clarka7890 <clarka7890@gmail.com>
2025-05-28 17:49:08 +0100

    added demo sequence to firmware

--- Motion Controller/STM32CubeIDE/servo_motion_controller/Core/Src/potentiometers.c
+++ Motion Controller/STM32CubeIDE/servo_motion_controller/Core/Src/potentiometers.c
@@ -15,26 +15,26 @@ void potentiometers_init()
     HAL_ADC_Start_DMA(&hadc1, pot_raw_value, POTS);
 
     // default values for white robot
-    pots[0].min_angle = -90.0;
-    pots[0].max_angle = 90.0;
-    pots[0].min_raw_value = 700.0;
-    pots[0].max_raw_value = 3300.0;
-
-    pots[1].min_angle = 0.0;
-    pots[1].max_angle = 90.0;
-    pots[1].min_raw_value = 720.0;
-    pots[1].max_raw_value = 2090.0;
+//    pots[0].min_angle = -90.0;
+//    pots[0].max_angle = 90.0;
+//    pots[0].min_raw_value = 700.0;
+//    pots[0].max_raw_value = 3300.0;
+//
+//    pots[1].min_angle = 0.0;
+//    pots[1].max_angle = 90.0;
+//    pots[1].min_raw_value = 720.0;
+//    pots[1].max_raw_value = 2090.0;
 
     // default values for blue robot
-//	pots[0].min_angle = -90.0;
-//	pots[0].max_angle = 90.0;
-//	pots[0].min_raw_value = 705.0;
-//	pots[0].max_raw_value = 3400.0;
-//
-//	pots[1].min_angle = 0.0;
-//	pots[1].max_angle = 90.0;
-//	pots[1].min_raw_value = 700.0;
-//	pots[1].max_raw_value = 2050.0;
+	pots[0].min_angle = -90.0;
+	pots[0].max_angle = 90.0;
+	pots[0].min_raw_value = 705.0;
+	pots[0].max_raw_value = 3400.0;
+
+	pots[1].min_angle = 0.0;
+	pots[1].max_angle = 90.0;
+	pots[1].min_raw_value = 700.0;
+	pots[1].max_raw_value = 2050.0;
 }
 
 float potentiometers_read_raw_value(int _channel)
